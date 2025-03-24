99176a120914540245aded8bb34a7115f118f00e
Stephen Asiedu <stephen.asiedu17@gmail.com>
2025-03-24 16:57:07 -0400

    app initialization working

--- app/__init__.py
+++ app/__init__.py
@@ -7,12 +7,19 @@ def create_app(config_name='default'):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     
+    @app.route("/")
+    def hello_world():
+        return "<h1>Welcome to STARGIGS , Your App is working!</h1>"
+    
+    
     db.init_app(app)  # Initialize SQLAlchemy
     ma.init_app(app)  # Initialize Marshmallow
 
     with app.app_context():
         db.create_all()  # Ensure tables are created
 
+    print()
     print('STARGIGS APP is Running')
+    print()
 
     return app
