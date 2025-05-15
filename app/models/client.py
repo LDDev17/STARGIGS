b88c7a79fcad71aab134413d103c3957ae0d80c5
dcerv005 <dacervantes12@gmail.com>
2025-05-15 10:16:29 -0700

    adding state, zip code, and profile pic

--- app/models/client.py
+++ app/models/client.py
@@ -13,4 +13,6 @@ class Client(db.Model):
     email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     phone: Mapped[int] = mapped_column(Integer, nullable=False, unique=True)
     city: Mapped[str] = mapped_column(String(100), nullable=False)
-    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    state: Mapped[str] = mapped_column(String(100), nullable=False)
+    zip_code: Mapped[int] = mapped_column(Integer(100), nullable=False)
+    profile_pic = Mapped[str] = mapped_column(String(500))
--- app/models/performer.py
+++ app/models/performer.py
@@ -13,6 +13,8 @@ class Performer(db.Model):
     email: Mapped[str] = mapped_column(String(255), nullable=False, unique=True)
     phone: Mapped[int] = mapped_column(Integer, nullable=False, unique=True)
     city: Mapped[str] = mapped_column(String(100), nullable=False)
-    password: Mapped[str] = mapped_column(String(255), nullable=False)
+    state: Mapped[str] = mapped_column(String(100), nullable=False)
+    zip_code: Mapped[int] = mapped_column(Integer(10), nullable=False)
+    profile_pic = Mapped[str] = mapped_column(String(500))
 
     gig_ads = relationship("GigAd", back_populates="performer", cascade="all, delete-orphan")
